32664fc760d91a0f977a5bdd6cc9bcfe4a436ef3
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-28 17:09:28 -0500

    new files

---
+++ src/nathaniel/ContactsManager.java
@@ -0,0 +1,21 @@
+package nathaniel;
+
+import java.util.*;
+import java.nio.file.*;
+import java.io.IOException;
+
+public class ContactsManager {
+
+//    Create a method for retrieving file contents.
+
+//    The method should accept a string that is the location
+//    of the file, and return a list of strings where each
+//    string is a line in the file.
+
+//    public static void main(String[] args) {
+//Path Files.write(Path out, List<String> lines[StandardOpenOption])
+//
+//    }
+
+
+}
